16fbb9275f57c762784570c89d16ee9db09aa995
jan-wong <543050768@qq.com>
2019-11-23 16:54:35 +0800

    compelete backbone todo app

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # backbone-todo
-todo applicatin by backbone
+
+#### MVC 与 backbone
+
+MVC这样的架构模式本意是在M层和V层中间插入一层C层用来解藕M层和V层，可以达到封装隔离，各自复用的目的。
+
+而Backbone在框架层面只拆分了M层和V层，V包含了Controller的功能。即使没有严格遵循MVC的模式，也不妨碍他取得的极大成功。
+
+why? 原因在于这样的设计理念给开发者在设计组件时带来了极大的灵活性。明知一些View没有复用的可能，干嘛要将C从V中拆分？写在一起代码不是更简洁吗？如果存在View需要复用时，开发者干嘛不可以自己拆分？完全可进可退
+
+#### TDDO Application
+
+![](https://raw.githubusercontent.com/jan-wong/backbone-todo/master/src/assets/01.png)
--- src/app.js
+++ src/app.js
@@ -1,34 +1,59 @@
 $(function(){
-  const InputView = Backbone.View.extend({
-    tagName: 'input',
-    className: 'todoo-input',
-    attributes: {
-      type: 'text'
-    },
-    events: {
-      'keydown': 'keyAction'
-    },
+
+  const listCollection = new Backbone.Collection([
+  ])
+
+  const TitleView = Backbone.View.extend({
+    tagName: 'div',
+
+    className: 'todo-title',
+
     initialize() {
-      console.log(this)
-    },
-    keyAction(e) {
-      if (e.which === 13) this.trigger('enter', e)
+      this.$el.html('TODO')
     }
   })
 
-  const listCollection = new Backbone.Collection([
-    { label: 'hello world' },
-    { label: 'hello pone' }
-  ])
+  const InputView = Backbone.View.extend({
+    tagName: 'input',
+
+    className: 'todo-input',
+
+    attributes: {
+      type: 'text',
+      placeholder: '输入待完成项目'
+    },
+
+    model: listCollection,
+
+    events: {
+      'keydown': 'keyAction'
+    },
+
+    keyAction(e) {
+      if (e.which === 13) {
+        this.model.add({
+          label: e.target.value,
+          checked: false
+        })
+        e.target.value = ''
+      }
+    }
+  })
 
   const ListView = Backbone.View.extend({
     model: listCollection,
   
     template: _.template($('#todo-list').html()),
+
+    events: {
+      'mousedown .todo-item_label': 'check',
+      'mousedown .todo-item_del': 'delete'
+    },
   
     initialize() {
       this.render()
       this.model.on('update', _ => this.render())
+      this.model.on('change', _ => this.render())
     },
   
     render() {
@@ -36,10 +61,19 @@ $(function(){
       return this
     },
 
-    addItem(label) {
-      this.model.add({
-        label: label
-      })
+    check(e) {
+      const index = this.$('.todo-item').index(
+        $(e.currentTarget).parent()
+      )
+      const checked = $(e.currentTarget).find('input').attr('checked')
+      this.model.at(index).set('checked', checked ? false : true)
+    },
+
+    delete(e) {
+      const index = this.$('.todo-item').index(
+        $(e.currentTarget).closest('.todo-item')
+      )
+      this.model.remove(this.model.at(index))
     }
   })
   
@@ -47,19 +81,13 @@ $(function(){
     el: '#todo',
   
     initialize() {
+      this.titleView = new TitleView()
       this.inputView = new InputView()
       this.listView = new ListView()
 
+      this.$el.append(this.titleView.$el)
       this.$el.append(this.inputView.$el)
       this.$el.append(this.listView.$el)
-
-      this.inputView.on('enter', e => {
-       this.listView.addItem(e.target.value)
-      })
-    },
-    
-    render() {
-      return this;
     }
   })
   
